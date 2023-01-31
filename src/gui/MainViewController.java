0f7dbddc4161c257dc89a674760753c0d351201d
Thiago Vanzele <thiagovanzele@gmail.com>
2023-01-31 18:12:15 -0300

    Design de exibição de lista de departamento

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartamentoAction() {
-		System.out.println("onMenuItemDepartamentoAction");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
