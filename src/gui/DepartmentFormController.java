5767ac0bc62b0c9a6222c7cc8b831179e4a46c59
Thiago Vanzele <thiagovanzele@gmail.com>
2023-02-03 17:07:22 -0300

    Salvando um novo departamento

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,18 +3,25 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
 	private Department entity;
 	
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtId;
@@ -32,17 +39,42 @@ public class DepartmentFormController implements Initializable {
 	private Button btCancel;
 	
 	@FXML
-	public void onBtSaveAction() {
-		System.out.println("Save");
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entidade é nula");
+		}
+		if (service == null ) {
+			throw new IllegalStateException("Serviço está nulo");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		} catch (DbException e) {
+			Alerts.showAlert("Error ao salvar o objeto", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
 	@FXML
-	public void onBtCacelAction() {
-		System.out.println("Cancel");
+	public void onBtCacelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	public void setDepartment (Department entity) {
 		this.entity = entity;
+	}
+	
+	public void setDepartmentService (DepartmentService service) {
+		this.service = service;
 	}
 	
 	@Override
@@ -60,7 +92,8 @@ public class DepartmentFormController implements Initializable {
 			throw new IllegalStateException("Entidade é nula!");
 		}
 		txtId.setText(String.valueOf(entity.getId()));
-		txtName.setText(entity.getName());
+		//txtName.setText(entity.getName());
+		txtId.setText( entity.getId() == null ? "" : String.valueOf(entity.getId()));
 	}
 
 }
