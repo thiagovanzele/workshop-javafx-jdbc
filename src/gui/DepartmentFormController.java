6854722aaf110703a9b38ab59ad2bc16a70c561b
Thiago Vanzele <thiagovanzele@gmail.com>
2023-02-04 22:10:16 -0300

    Padrão de desgin do oberserver para atualizar a visualização da tabela

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,66 +1,78 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
 	private Department entity;
-	
+
 	private DepartmentService service;
-	
+
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+
 	@FXML
 	private TextField txtId;
-	
+
 	@FXML
 	private TextField txtName;
-	
+
 	@FXML
 	private Label labelErrorName;
-	
+
 	@FXML
 	private Button btSave;
-	
+
 	@FXML
 	private Button btCancel;
-	
+
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
 		if (entity == null) {
 			throw new IllegalStateException("Entidade é nula");
 		}
-		if (service == null ) {
+		if (service == null) {
 			throw new IllegalStateException("Serviço está nulo");
 		}
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		} catch (DbException e) {
 			Alerts.showAlert("Error ao salvar o objeto", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
+
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+	}
+
 	private Department getFormData() {
 		Department obj = new Department();
-		
+
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
 		obj.setName(txtName.getText());
-		
+
 		return obj;
 	}
 
@@ -68,32 +80,36 @@ public class DepartmentFormController implements Initializable {
 	public void onBtCacelAction(ActionEvent event) {
 		Utils.currentStage(event).close();
 	}
-	
-	public void setDepartment (Department entity) {
+
+	public void setDepartment(Department entity) {
 		this.entity = entity;
 	}
-	
-	public void setDepartmentService (DepartmentService service) {
+
+	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
-	
+
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		
+
 	}
-	
+
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
 	}
-	
+
 	public void updateFormData() {
 		if (entity == null) {
 			throw new IllegalStateException("Entidade é nula!");
 		}
 		txtId.setText(String.valueOf(entity.getId()));
-		//txtName.setText(entity.getName());
-		txtId.setText( entity.getId() == null ? "" : String.valueOf(entity.getId()));
+		// txtName.setText(entity.getName());
+		txtId.setText(entity.getId() == null ? "" : String.valueOf(entity.getId()));
 	}
 
 }
